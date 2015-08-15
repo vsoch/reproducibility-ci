1d7f892af558594603877149baecda89c2a7f3bc
vsoch <vsochat@stanford.edu>
2015-08-15 16:53:39 -0700

    adding R script

---
+++ analysis/prep_cooccurr_data.R
@@ -0,0 +1,27 @@
+require(reshape)
+require(plyr)
+require(pheatmap)
+
+indir = "output"
+input_datas = list.files(indir,pattern="*.tsv")
+outdir = "output"
+
+for (input_data in input_datas){
+    input_file = paste(indir,"/",input_data,sep="")
+    output_file = paste(outdir,"/",input_data,sep="")  
+    df = read.csv(input_file,sep="\t",row.names=1)  
+    # Melt into data frame
+    flat = melt(as.matrix(df))
+    x = c()
+    y = c()
+    for (dx in 1:nrow(df)){
+      for (dy in 1:ncol(df)){
+        x = c(x,dx)
+        y = c(y,dy)
+      }
+    }
+    flat$x = x
+    flat$y = y
+    colnames(flat) = c("probof","given","prob","x","y")
+    write.table(flat,file=output_file,sep="\t",row.names=FALSE,col.names=TRUE,quote=FALSE)    
+}
